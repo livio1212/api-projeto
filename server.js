cfdcfa68256754e610daa4175f11e6af62ac0792
livio1212 <liviocosta229@gmail.com>
2025-03-06 10:18:05 -0300

    .

--- server.js
+++ server.js
@@ -1,71 +1,33 @@
-const express = require("express");
-const app = express();
-const data = require("./data.json");
+import express from 'express'
+import { PrimsaClient } from '@prisma/client'
 
-
-//verbos HTTP
-
-// GET : RECEBER DADOS DE UM RESORCE
-// POST: ENVIAR DADOS OU INFORMAÇOES PARA PARA SEREM PRCESSADOS POR UM RESORCE
-// PUT: ATUALIZA DADOS DE UM RESORCE
-// DELETE: DELETAR UM RESORCE
+const = prisma = new PrismaClient()
+ 
+const app = express() 
 
 app.use(express.json());
 
-app.get("/clients", function(req, res){
-    res.json(data);
-});
 
+const users = []
 
+app.post('/user', (req, res) => {
 
-app.get("/clients/:id/", function(req, res){
+    prisma.user.create({
+        data: 
+        email: req.body.email,
+        name: req.body.name,
+        age:req.body.age
+        
+    })
+    res.status(201, 'Deu certo')
+})
 
-    const { id } = req.params;
-    const client = data.find(cli => cli.id == id);
-
-    if (!client) return res.status(204).json();
-
-    res.json(client);
-
-});
-app.post("/clients", function(req, res){
-
-
-    const {name, email} = req.body;
-
-    //salvar novo cliente
-
-    res.json({name, email});
-
-
-});
-app.put("/clients/:id", function(req, res){
-
-
-    const { id } = req.params;
-    const client = data.find(cli => cli.id == id);
-
+app.get('/user', (req, res) => {
     
-    if (!client) return res.status(204).json();
-
-    const { name } = req.body;
-
-    client.name = name;
-
-    res.json(client);
-
-});
-app.delete("/clients/:id", function(req, res){
-
-    const { id } = req.params;
-    const clientFiltered = data.filter(client => client.id != id);
-
-
-    res.json(clientFiltered);
+    res.status(200).json(users);
 
 });
 
 
-app.listen(3000, function(){
-    console.log("Server is runnig");
-});
+app.listen(3000)
+
